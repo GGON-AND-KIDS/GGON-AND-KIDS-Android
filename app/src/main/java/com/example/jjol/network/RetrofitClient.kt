92ea6c89cb1f1335350d6c380c2dc4c6f6d159b3
kimjjunho <kimjjun1007@gmail.com>
2023-01-15 00:10:41 +0900

    feat: retrofit builder

---
+++ app/src/main/java/com/example/jjol/network/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.example.jjol.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient {
+    private val baseURI = ""
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(baseURI)
+        .client(okHttpClient())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+}
+
+private fun okHttpClient(): OkHttpClient {
+    val httpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    val authorizationInterceptor =
+
+    return OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+}
